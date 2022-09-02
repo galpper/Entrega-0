d6a12edc60edce82651db6c7b69e4c4ebbd37f6b
galpper <guzmanperera@outlook.com>
2022-09-02 14:02:03 -0300

    Act. user

--- js/init.js
+++ js/init.js
@@ -40,3 +40,68 @@ let getJSONData = function(url){
         return result;
     });
 }
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  
+  let usuario = sessionStorage.getItem("user");
+  if (usuario==null){
+      Swal.fire({
+          position: 'center',
+          icon: 'warning',
+          title: 'Desea iniciar sesión?',
+          showConfirmButton: true,
+          showCancelButton: true,
+          confirmButtonColor: 'rgb(55, 217, 50)',
+          cancelButtonColor: '#d33',
+          confirmButtonText:"Iniciar sesión",
+          cancelButtonText:"En otro momento",
+      }).then((result) => {
+  if (result.isConfirmed) {
+  location.href="login.html";
+  }})
+  document.getElementById("navbarNav").innerHTML += `<ul class="navbar-nav w-100 justify-content-between">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.html">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="categories.html">Categorías</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="sell.html">Vender</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="login.html">Iniciar sesión</a>
+        </li>
+      </ul>`
+  } else if (usuario != null){
+    document.getElementById("navbarNav").innerHTML +=
+    `<ul class="navbar-nav w-100 justify-content-between">
+    <li class="nav-item">
+      <a class="nav-link active" href="index.html">Inicio</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="categories.html">Categorías</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="sell.html">Vender</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="my-profile.html">${usuario}</a>
+    </li>
+    </ul>`
+  }
+  else {
+      document.getElementById("navbarNav").innerHTML += `<ul class="navbar-nav w-100 justify-content-between">
+      <li class="nav-item">
+        <a class="nav-link active" href="index.html">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="categories.html">Categorías</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="sell.html">Vender</a>
+      </li>
+      </ul>`
+  }
+  });
+  
