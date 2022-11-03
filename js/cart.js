8798c0a3d57a04fab4468b7c1a4cf866f3ac7da2
galpper <guzmanperera@outlook.com>
2022-11-02 22:26:08 -0300

    02NOV

--- js/cart.js
+++ js/cart.js
@@ -6,8 +6,101 @@ let envioTotal = 0;
 let costoEnvio = 0;
 let total = 0;
 let algo = document.getElementsByClassName("cant"); // algo.cant.value
+let radio1 = "";
+let radio2 = "";
+
+// funcion de validacion
+function validar(){
+    let cantidad = document.getElementById("cant");
+    let envio1 = document.getElementById("envio1");
+    let envio2 = document.getElementById("envio2");
+    let envio3 = document.getElementById("envio3");
+    let calle = document.getElementById("calle");
+    let num = document.getElementById("numcasa");
+    let esq = document.getElementById("esq");
+    let modalcredito = document.getElementById("credito");
+    let modalbanco = document.getElementById("banco");
+    let reultadoValidacion = true;
+    /*
+    let numtarjeta = document.getElementById("numtarjeta");
+    let codigo = document.getElementById("codigo");
+    let vencimiento = document.getElementById("vencimiento");
+    let numbanco = document.getElementById("numbanco");
+*/
+   /* if (exprecionRegularEmail.test(correoElecronico.value)){
+        correoElecronico.setCustomValidity("");
+    }else{
+        correoElecronico.setCustomValidity(false);
+        reultadoValidacion=false;
+    }
+  
+    if (contrasenia.value != confirmarContrasenia.value || !contrasenia.checkValidity()){
+        confirmarContrasenia.setCustomValidity(false);
+        reultadoValidacion =false;
+    } else {
+        confirmarContrasenia.setCustomValidity(""); //Se muestra la clase invalid-color
+    }*/
+    if (cantidad.value>=1){
+        cantidad.setCustomValidity("");
+    }else{
+        cantidad.setCustomValidity(false);
+        reultadoValidacion=false;
+    }
+    if (calle.innerHTML == ""){
+        calle.setCustomValidity("");
+    }else{
+        calle.setCustomValidity(false);
+        reultadoValidacion=false;
+    }
+    if (esq.innerHTML == ""){
+        esq.setCustomValidity("");
+    }else{
+        esq.setCustomValidity(false);
+        reultadoValidacion=false;
+    }
+    if (num.innerHTML == ""){
+        num.setCustomValidity("");
+    }else{
+        num.setCustomValidity(false);
+        reultadoValidacion=false;
+    }
+    if (!modalcredito.checked || !modalbanco.checked){
+        modalbanco.setCustomValidity(false);
+        modalcredito.setCustomValidity(false);
+        reultadoValidacion=false;
+    }else{
+        modalbanco.setCustomValidity("");
+        modalcredito.setCustomValidity("");
+    }
+    if (!envio1.checked || !envio2.checked || !envio3.checked ){
+        envio1.setCustomValidity(false);
+        envio2.setCustomValidity(false);
+        envio3.setCustomValidity(false);
+        reultadoValidacion=false;
+    }else{
+        envio1.setCustomValidity("");
+        envio2.setCustomValidity("");
+        envio3.setCustomValidity("");
+    }
+    console.log('Validado!!!!')
+    return reultadoValidacion
+    
+}
+/*
+
+        numtarjeta.setCustomValidity(false);
+        codigo.setCustomValidity(false);
+        vencimiento.setCustomValidity(false);
+        numbanco.setCustomValidity(false);
 
 
+        numtarjeta.setCustomValidity("");
+        codigo.setCustomValidity("");
+        vencimiento.setCustomValidity("");
+        numbanco.setCustomValidity("");
+
+
+*/
 
 function suma(array) {
     let cant = document.getElementById("cant").value;
@@ -64,7 +157,35 @@ function showArt(arrayProduct) {
 }
 
 
+function tipoCredito(radio1, radio2) {
+ radio1 = document.getElementById("credito").checked;
+ radio2 = document.getElementById("banco").checked;
 
+    if (radio1==true) {
+        document.getElementById("tarjeta").innerHTML = `
+        <div class="col-5">
+        <label for="">Número de tarjeta:
+        <input id="numtarjeta" required class="form-control" type="text"></label>
+        </div>
+        <div class="col-4">
+        <label for="">Codigo seg.:<input id="codigo" required class="form-control" type="text"></label>
+        </div>
+        <div class="col-5">
+        <label for="">Vencimiento (MM/AA): <input id="vencimiento" required class="form-control" type="text"></label>
+        </div>
+        `;
+        document.getElementById("numbanco").innerHTML = "";
+
+        } else if (radio2==true) {
+           
+            document.getElementById("numbanco").innerHTML =  `
+            <label for="">Número de cuenta: <input id="numbanco" required class="form-control" type="text"></label>
+            `;
+            document.getElementById("tarjeta").innerHTML = "";
+        } else if (radio1==false){
+            document.getElementById("tarjeta").innerHTML = "";
+        }
+}
 
 
 
@@ -83,6 +204,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 
+    
 
     document.getElementById("envio1").addEventListener("change", ()=>{
         envio(en1, costoEnvio);
@@ -99,5 +221,28 @@ document.addEventListener("DOMContentLoaded", () => {
         sessionStorage.setItem("carrito", en3);
     });
 
+    document.getElementById("credito").addEventListener("click", ()=>{
+        tipoCredito(radio1, radio2);
+    });
+
+    document.getElementById("banco").addEventListener("click", ()=>{
+        tipoCredito(radio1, radio2);
+    });
+
+    document.getElementById('formcart').addEventListener('submit', evento=>{
+ 
+        if( !validar() || !this.checkValidity() ){
+            evento.preventDefault();
+            evento.stopPropagation();
+        }
+        
+        document.body.classList.add('was-validated');
+      
+        let eventos=['change', 'input'];
+        
+        eventos.forEach( evento=> {document.body.addEventListener(evento, validar)})
+         
+      
+      })
     
 });
